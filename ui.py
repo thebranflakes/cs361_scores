20233153c33ba547ee39af0b660bda1ca1a2ba47
thebranflakes <hernanb2@oregonstate.edu>
2022-10-10 20:11:56 -0400

    nfl team's added.

--- ui.py
+++ ui.py
@@ -1,14 +1,17 @@
 import random
 
+NFL = ["ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE", "DAL", "DEN", "DET",
+       "GB", "HOU", "IND", "JAX", "KC", "MIA", "MIN", "NE", "NO", "NYG", "NYJ", "LV",
+       "PHI", "PIT", "LAC", "SF", "SEA", "LAR", "TB", "TEN", "WAS"]
 
 
 while True:
-
+    team_choice = ""
     league_input = input("Enter 1 for NBA scores or 2 for NFL scores:\n")
 
     while league_input not in ["1", "2"]:
         print("That is not a correct input, try again.\n")
-        break
+        league_input = input("Enter 1 for NBA scores or 2 for NFL scores:\n")
 
     if league_input == "1":
         print("NBA it is!\n")
@@ -17,6 +20,18 @@ while True:
     if league_input == "2":
         print("NFL it is!\n")
         team_choice = input("Which teams score did you want to check?\n")
+
+    while team_choice not in NFL:
+        team_choice = input("That isn't a team option, try again. Enter 1 for the list of teams or 2 to restart:\n")
+        if team_choice == "1":
+            print(NFL)
+            team_choice = input("Which teams score did you want to check?:\n")
+        if team_choice == "2":
+            break
+
+
+
+
 
 
 
