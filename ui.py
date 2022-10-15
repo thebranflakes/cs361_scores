c1ddc52e05736057ba4411ec1bdbd4c0f331e449
thebranflakes <hernanb2@oregonstate.edu>
2022-10-15 14:00:23 -0400

    NBA scoring updated and working, random scoring is up next.

--- ui.py
+++ ui.py
@@ -11,7 +11,7 @@ NBA = ['ATL', 'BOS', 'BKN', 'CHA', 'CHI', 'CLE', 'DET', 'GS', 'IND', 'LAL', 'MIA
 while True:
     team_choice = ""
     league_input = input("Enter 1 for NBA scores or 2 for NFL scores (You will need to pick a team)\n"
-                         "(New Feature) Get a score fast! Random NBA or NFL score, just enter 3!\n ")
+                         "(New Feature) Get a score fast! Random NBA or NFL score, just enter 3!\n")
 
     while league_input not in ["1", "2", "3"]:
         print("That is not a correct input, try again.\n")
@@ -24,6 +24,42 @@ while True:
                             "OR\n"
                             "Press 1 for List of teams\n"
                             "Press 2 to restart\n")
+
+        while team_choice not in NBA:
+            if team_choice == "1":
+                print(NBA)
+                team_choice = input("Which teams score did you want to check?:\n")
+            if team_choice == "2":
+                break
+
+        if team_choice in NBA:
+
+            quarter = random.randint(1, 5)
+            opponent = NBA[random.randint(0, 29)]
+            team_score = random.randint(2, 70)
+            opp_score = random.randint(2, 70)
+            winner = ""
+
+            if team_score > opp_score:
+                winner = team_choice
+            if opp_score > team_score:
+                winner = opponent
+            while opponent == team_choice:
+                opponent = NBA[random.randint(0, 29)]
+
+            print(team_choice + ": " + str(team_score))
+            print(opponent + ": " + str(opp_score))
+
+            if quarter in [1, 2, 3, 4]:
+                print("Quarter: " + str(quarter))
+                print(winner + " is winning!")
+            if quarter > 4:
+                print("Final")
+                print(winner + " won!")
+
+            what_next = input("\nEnter 1 EXIT or any key to restart:\n")
+            if what_next == "1":
+                break
 
     if league_input == "2":
         print("NFL it is!\n")
@@ -71,11 +107,6 @@ while True:
 
     if league_input == "3":
         print("Here is your random score!")
-        what_next = input("\nEnter 1 EXIT or any key to restart:\n")
+        what_next = input("\nEnter 1 to EXIT or any other key to restart:\n")
         if what_next == "1":
             break
-
-
-
-
-
