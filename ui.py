6baeb55b3d039a1ef014cacb566a725b0109242b
thebranflakes <hernanb2@oregonstate.edu>
2022-10-15 01:24:46 -0400

    Added NBA list, will implement the scores next push.

--- ui.py
+++ ui.py
@@ -4,6 +4,8 @@ NFL = ["ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE", "DAL", "DEN", "DE
        "GB", "HOU", "IND", "JAX", "KC", "MIA", "MIN", "NE", "NO", "NYG", "NYJ", "LV",
        "PHI", "PIT", "LAC", "SF", "SEA", "LAR", "TB", "TEN", "WAS"]
 
+NBA = ['ATL', 'BOS', 'BKN', 'CHA', 'CHI', 'CLE', 'DET', 'GS', 'IND', 'LAL', 'MIA', 'NO', 'NY', 'OKC', 'ORL', 'PHX',
+       'POR', 'SAC', 'SA', 'TOR', 'UTAH', 'WSH', 'DAL', 'DEN', 'HOU', 'LAC', 'MEM', 'MIL', 'MIN', 'PHI']
 
 while True:
     team_choice = ""
@@ -21,43 +23,42 @@ while True:
         print("NFL it is!\n")
         team_choice = input("Which teams score did you want to check?\n")
 
-    while team_choice not in NFL:
-        team_choice = input("That isn't a team option, try again. Enter 1 for the list of teams or 2 to restart:\n")
-        if team_choice == "1":
-            print(NFL)
-            team_choice = input("Which teams score did you want to check?:\n")
-        if team_choice == "2":
-            break
+        while team_choice not in NFL:
+            team_choice = input("That isn't a team option, try again. Enter 1 for the list of teams or 2 to restart:\n")
+            if team_choice == "1":
+                print(NFL)
+                team_choice = input("Which teams score did you want to check?:\n")
+            if team_choice == "2":
+                break
 
-    if team_choice in NFL:
+        if team_choice in NFL:
 
-        quarter = random.randint(1, 5)
-        opponent = NFL[random.randint(0, 31)]
-        team_score = random.randint(2, 70)
-        opp_score = random.randint(2, 70)
-        winner = ""
-
-        if team_score > opp_score:
-            winner = team_choice
-        if opp_score > team_score:
-            winner = opponent
-        while opponent == team_choice:
+            quarter = random.randint(1, 5)
             opponent = NFL[random.randint(0, 31)]
+            team_score = random.randint(2, 70)
+            opp_score = random.randint(2, 70)
+            winner = ""
 
-        print(team_choice + ": " + str(team_score))
-        print(opponent + ": " + str(opp_score))
+            if team_score > opp_score:
+                winner = team_choice
+            if opp_score > team_score:
+                winner = opponent
+            while opponent == team_choice:
+                opponent = NFL[random.randint(0, 31)]
 
-        if quarter in [1, 2, 3, 4]:
-            print("Quarter: " + str(quarter))
-            print(winner + " is winning!")
-        if quarter > 4:
-            print("Final")
-            print(winner + " won!")
+            print(team_choice + ": " + str(team_score))
+            print(opponent + ": " + str(opp_score))
 
-        what_next = input("\nEnter 1 EXIT or any key to restart:\n")
-        if what_next == "1":
-            break
+            if quarter in [1, 2, 3, 4]:
+                print("Quarter: " + str(quarter))
+                print(winner + " is winning!")
+            if quarter > 4:
+                print("Final")
+                print(winner + " won!")
 
+            what_next = input("\nEnter 1 EXIT or any key to restart:\n")
+            if what_next == "1":
+                break
 
 
 
