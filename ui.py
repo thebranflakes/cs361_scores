6cb42e80171d9dc892b5ef37cfcaad610a092373
thebranflakes <hernanb2@oregonstate.edu>
2022-11-16 15:18:11 -0500

    Starting some refactoring to make the code smell less.

--- ui.py
+++ ui.py
@@ -9,32 +9,55 @@ NBA = ['ATL', 'BOS', 'BKN', 'CHA', 'CHI', 'CLE', 'DET', 'GS', 'IND', 'LAL', 'MIA
        'NO', 'NY', 'OKC', 'ORL', 'PHX', 'POR', 'SAC', 'SA', 'TOR', 'UTAH', 'WSH',
        'DAL', 'DEN', 'HOU', 'LAC', 'MEM', 'MIL', 'MIN', 'PHI']
 
+
+def league_choice():
+    while True:
+        league = input("Enter 1 for NBA scores or 2 for NFL scores (You will need to pick a team)\n"
+                       "(New Feature) Get a score fast! Random NBA or NFL score, just enter 3!\n")
+        if league not in ["1", "2", "3"]:
+            print("That is not a correct input, try again.\n")
+        if league in ["1", "2", "3"]:
+            return league
+
+
+def team_choice(league):
+    return None
+
+
+def microservice():
+    return None
+
+
 while True:
 
-    team_choice = ""
-    league_input = input("Enter 1 for NBA scores or 2 for NFL scores (You will need to pick a team)\n"
-                         "(New Feature) Get a score fast! Random NBA or NFL score, just enter 3!\n")
+    league_input = league_choice()
 
-    while league_input not in ["1", "2", "3"]:
-        print("That is not a correct input, try again.\n")
-        league_input = input("Enter 1 for NBA scores or 2 for NFL scores:\n"
-                             "or 3 for random score.\n")
+    if league_input == "1":
+        league_input = NBA
+
+    if league_input == "2":
+        league_input = NFL
+
+    if league_input == "3":
+        microservice()
+
+    team_input = team_choice(league_input)
 
     if league_input == "1":
         print("NBA it is!\n")
-        team_choice = input("Which teams score did you want to check?\n"
+        team_input = input("Which teams score did you want to check?\n"
                             "OR\n"
                             "Press 1 for List of teams\n"
                             "Press 2 to restart\n")
 
-        while team_choice not in NBA:
-            if team_choice == "1":
+        while team_input not in NBA:
+            if team_input == "1":
                 print(NBA)
-                team_choice = input("Which teams score did you want to check?:\n")
-            if team_choice == "2":
+                team_input = input("Which teams score did you want to check?:\n")
+            if team_input == "2":
                 break
 
-        if team_choice in NBA:
+        if team_input in NBA:
 
             quarter = random.randint(1, 5)
             opponent = NBA[random.randint(0, 29)]
@@ -65,19 +88,19 @@ while True:
 
     if league_input == "2":
         print("NFL it is!\n")
-        team_choice = input("Which teams score did you want to check? (Enter team abbreviation)\n"
+        team_input = input("Which teams score did you want to check? (Enter team abbreviation)\n"
                             "OR\n"
                             "Press 1 for List of teams\n"
                             "Press 2 to restart\n")
 
-        while team_choice not in NFL:
-            if team_choice == "1":
+        while team_input not in NFL:
+            if team_input == "1":
                 print(NFL)
-                team_choice = input("Which teams score did you want to check?:\n")
-            if team_choice == "2":
+                team_input = input("Which teams score did you want to check?:\n")
+            if team_input == "2":
                 break
 
-        if team_choice in NFL:
+        if team_input in NFL:
 
             quarter = random.randint(1, 5)
             opponent = NFL[random.randint(0, 31)]
@@ -86,13 +109,13 @@ while True:
             winner = ""
 
             if team_score > opp_score:
-                winner = team_choice
+                winner = team_input
             if opp_score > team_score:
                 winner = opponent
-            while opponent == team_choice:
+            while opponent == team_input:
                 opponent = NFL[random.randint(0, 31)]
 
-            print(team_choice + ": " + str(team_score))
+            print(team_input + ": " + str(team_score))
             print(opponent + ": " + str(opp_score))
 
             if quarter in [1, 2, 3, 4]:
@@ -127,7 +150,7 @@ while True:
 
         # print out the number recieved
         print(micro_num)
-        
+
         what_next = input("\nEnter 1 to EXIT or any other key to restart:\n")
         if what_next == "1":
             break
