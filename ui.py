5a1ab29a04f7be30e60b93ce21940010bf66ea1c
thebranflakes <hernanb2@oregonstate.edu>
2022-11-17 12:02:17 -0500

    Code looks a lot cleaner after refactoring. Moved code into functions instead of one huge while loop. Could look at get_score function as a code smell, but I want to keep all of that in one function.

--- ui.py
+++ ui.py
@@ -10,6 +10,10 @@ NBA = ['ATL', 'BOS', 'BKN', 'CHA', 'CHI', 'CLE', 'DET', 'GS', 'IND', 'LAL', 'MIA
        'DAL', 'DEN', 'HOU', 'LAC', 'MEM', 'MIL', 'MIN', 'PHI']
 
 
+def prog_exit():
+    exit()
+
+
 def league_choice():
     while True:
         league = input("Enter 1 for NBA scores or 2 for NFL scores (You will need to pick a team)\n"
@@ -21,11 +25,74 @@ def league_choice():
 
 
 def team_choice(league):
-    return None
+    while True:
+        team = input("Which teams score did you want to check?\n"
+                     "OR\n"
+                     "Enter 1 for List of teams\n"
+                     "Enter 2 to restart\n"
+                     "Enter 3 to Exit\n")
+        if team == "1":
+            print(league)
+        if team in league or team == "2" or team == "3":
+            if team == "3":
+                prog_exit()
+            return team
 
 
-def microservice():
-    return None
+def get_score():
+    quarter = random.randint(1, 5)
+    opponent = league_input[random.randint(0, len(league_input) - 1)]
+    team_score = random.randint(2, league_max)
+    opp_score = random.randint(2, league_max)
+    winner = ""
+
+    if team_score > opp_score:
+        winner = team_input
+    if opp_score > team_score:
+        winner = opponent
+    while opponent == team_input:
+        opponent = league_input[random.randint(0, len(league_input) - 1)]
+
+    print(team_input + ": " + str(team_score))
+    print(opponent + ": " + str(opp_score))
+
+    if quarter in [1, 2, 3, 4]:
+        print("Quarter: " + str(quarter))
+        print(winner + " is winning!")
+    if quarter > 4:
+        print("Final")
+        print(winner + " won!")
+
+    what_next = input("\nEnter 1 EXIT or any key to restart:\n")
+    if what_next == "1":
+        prog_exit()
+
+
+def micro():
+    # Open file for writing
+    microservice = open("micro.txt", "w")
+    time.sleep(1)
+    print("Receiving Data from Microservice...\n")
+
+    # Write micro into txt file
+    microservice.write("micro")
+    time.sleep(3)
+    microservice.close()
+
+    # Open file for reading
+    microservice = open("micro.txt", "r")
+    time.sleep(3)
+    micro_num = microservice.read()
+    microservice.close()
+
+    print("Data Received, Generating Score...\n")
+
+    # print out the number received
+    print(micro_num)
+
+    what_next = input("\nEnter 1 to EXIT or any other key to restart:\n")
+    if what_next == "1":
+        prog_exit()
 
 
 while True:
@@ -34,123 +101,18 @@ while True:
 
     if league_input == "1":
         league_input = NBA
+        league_max = 120
 
     if league_input == "2":
         league_input = NFL
+        league_max = 55
 
     if league_input == "3":
-        microservice()
+        micro()
 
     team_input = team_choice(league_input)
 
-    if league_input == "1":
-        print("NBA it is!\n")
-        team_input = input("Which teams score did you want to check?\n"
-                            "OR\n"
-                            "Press 1 for List of teams\n"
-                            "Press 2 to restart\n")
+    if team_input == "2":
+        break
 
-        while team_input not in NBA:
-            if team_input == "1":
-                print(NBA)
-                team_input = input("Which teams score did you want to check?:\n")
-            if team_input == "2":
-                break
-
-        if team_input in NBA:
-
-            quarter = random.randint(1, 5)
-            opponent = NBA[random.randint(0, 29)]
-            team_score = random.randint(2, 70)
-            opp_score = random.randint(2, 70)
-            winner = ""
-
-            if team_score > opp_score:
-                winner = team_choice
-            if opp_score > team_score:
-                winner = opponent
-            while opponent == team_choice:
-                opponent = NBA[random.randint(0, 29)]
-
-            print(team_choice + ": " + str(team_score))
-            print(opponent + ": " + str(opp_score))
-
-            if quarter in [1, 2, 3, 4]:
-                print("Quarter: " + str(quarter))
-                print(winner + " is winning!")
-            if quarter > 4:
-                print("Final")
-                print(winner + " won!")
-
-            what_next = input("\nEnter 1 EXIT or any key to restart:\n")
-            if what_next == "1":
-                break
-
-    if league_input == "2":
-        print("NFL it is!\n")
-        team_input = input("Which teams score did you want to check? (Enter team abbreviation)\n"
-                            "OR\n"
-                            "Press 1 for List of teams\n"
-                            "Press 2 to restart\n")
-
-        while team_input not in NFL:
-            if team_input == "1":
-                print(NFL)
-                team_input = input("Which teams score did you want to check?:\n")
-            if team_input == "2":
-                break
-
-        if team_input in NFL:
-
-            quarter = random.randint(1, 5)
-            opponent = NFL[random.randint(0, 31)]
-            team_score = random.randint(2, 70)
-            opp_score = random.randint(2, 70)
-            winner = ""
-
-            if team_score > opp_score:
-                winner = team_input
-            if opp_score > team_score:
-                winner = opponent
-            while opponent == team_input:
-                opponent = NFL[random.randint(0, 31)]
-
-            print(team_input + ": " + str(team_score))
-            print(opponent + ": " + str(opp_score))
-
-            if quarter in [1, 2, 3, 4]:
-                print("Quarter: " + str(quarter))
-                print(winner + " is winning!")
-            if quarter > 4:
-                print("Final")
-                print(winner + " won!")
-
-            what_next = input("\nEnter 1 EXIT or any key to restart:\n")
-            if what_next == "1":
-                break
-
-    if league_input == "3":
-        # Open file for writing
-        microservice = open("micro.txt", "w")
-        time.sleep(1)
-        print("Receiving Data from Microservice...\n")
-
-        # Write micro into txt file
-        microservice.write("micro")
-        time.sleep(3)
-        microservice.close()
-
-        # Open file for reading
-        microservice = open("micro.txt", "r")
-        time.sleep(3)
-        micro_num = microservice.read()
-        microservice.close()
-
-        print("Data Received, Generating Score...\n")
-
-        # print out the number recieved
-        print(micro_num)
-
-        what_next = input("\nEnter 1 to EXIT or any other key to restart:\n")
-        if what_next == "1":
-            break
+    get_score()
